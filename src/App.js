3932dbac7a28b6ddf11c0d10337153496b8a6561
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-01-05 14:58:34 +0100

    upgrade react-scripts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Page from './components/Page.js'
+import './App.css'
+
+import Home from './components/pages/Home.js'
+import Works from './components/pages/Works.js'
+import Skills from './components/pages/Skills.js'
+import Contact from './components/pages/Contact.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handlerContent = this.handlerContent.bind(this);
+    
+    this.state = { 
+      currentPage: 'Home',
+      content: {
+        header: {
+          title: 'Welcome to my web portfolio' ,
+          subtitle: '— Ideate romantically, create pragmatically, design everything you do. —'
+        },
+        body: 'This is Home',
+        footer: false
+      },
+      pages: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/works',
+            name: 'Works',
+            component: Works
+        },
+        {
+            path: '/skills',
+            name: 'Skills',
+            component: Skills
+        },
+        {
+            path: '/contact',
+            name: 'Contact',
+            component: Contact
+        }
+      ]
+    };
+  }
+
+  handlerContent(pathname) {
+    let { content, currentPage } = this.state;
+
+    switch(pathname) {
+      case '/':
+        currentPage = 'Home';
+        content = {
+          header: {
+            title: 'Welcome to my web portfolio' ,
+            subtitle: '— Ideate romantically, create pragmatically, design everything you do. —'
+          },
+          body: 'This is Home'
+        };
+        break;
+      case '/contact':
+        currentPage = 'Contact';
+        content = {
+          header: {
+            title: 'Welcome to my contact' ,
+            subtitle: '— Ideate romantically, create pragmatically, design everything you do. —'
+          },
+          body: 'This is Contact'
+        };
+        break;
+      case '/works':
+        currentPage = 'Works';
+        content = {
+          header: {
+            title: 'Find out all the information about my work experience in web development' ,
+            subtitle: '— Ideate romantically, create pragmatically, design everything you do. —'
+          },
+          body: 'This is My work experience'
+        }
+        break;
+      case '/skills':
+        currentPage = 'skills';
+        content = {
+          header: {
+            title: 'Have a look and discover the information about my personal skills, my academic experience and my technical abilities' ,
+            subtitle: ''
+          },
+          body: 'This is My skills'
+        }
+        break;
+      default:
+    }
+    this.setState({
+      currentPage,
+      content
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Page name={this.state.currentPage} content={this.state.content} pages={this.state.pages} setContent={this.handlerContent}/>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default App
