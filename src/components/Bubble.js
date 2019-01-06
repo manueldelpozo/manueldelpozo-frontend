aa11bb5e247e31a1e7d2e5105e38ca26066a58fe
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-01-06 23:51:20 +0100

    fix: round bubbles

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon'
-import './Bubble.css'
 
 const BubbleButton = withStyles({
     root: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        borderRadius: 3,
+        borderRadius: 50,
         border: 0,
         color: 'white',
         padding: '0 30px',
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        height: 'auto'
+        height: 100,
+        width: 100
     },
     label: {
         textTransform: 'capitalize',
