b71ac139ee4a4f306783036588115753a422a93f
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-01-14 22:01:36 +0100

    Fix contact link

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch, NavLink as Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import Header from './Header.js'
 import Main from './Main.js'
 
